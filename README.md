62ad97198c72181369454931c69169fa3b67e27e
Maxim Rybetsky <Maksim.Rybetsky@raiffeisen.ru>
2024-04-02 13:50:48 +0300

    Step 5: Add Even number game

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 [![Actions Status](https://github.com/MaxRybetsky/java-project-61/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/MaxRybetsky/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/b811ecb01b310bea4dce/maintainability)](https://codeclimate.com/github/MaxRybetsky/java-project-61/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/b811ecb01b310bea4dce/test_coverage)](https://codeclimate.com/github/MaxRybetsky/java-project-61/test_coverage)
+
+### Even Number Game
+[![asciicast](https://asciinema.org/a/SY3CZf2c1HxfD1El9Gf7Yftct.svg)](https://asciinema.org/a/SY3CZf2c1HxfD1El9Gf7Yftct)
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,21 +3,27 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void run() {
+    private static final String WELCOME_MESSAGE = "Welcome to the Brain Games!";
+    private static final String NAME_QUESTION = "May I have your name? ";
+    private static final String GREETING_MESSAGE_TEMPLATE = "Hello, %s!\n";
+
+    public static String greetAndReturnUserName() {
         printGreeting();
 
         askName();
         var userName = getNameFromInput();
 
         sayHelloWithName(userName);
+
+        return userName;
     }
 
     private static void printGreeting() {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println(WELCOME_MESSAGE);
     }
 
     private static void askName() {
-        System.out.print("May I have your name? ");
+        System.out.print(NAME_QUESTION);
     }
 
     private static String getNameFromInput() {
@@ -27,6 +33,6 @@ public class Cli {
     }
 
     private static void sayHelloWithName(String userName) {
-        System.out.printf("Hello, %s!\n", userName);
+        System.out.printf(GREETING_MESSAGE_TEMPLATE, userName);
     }
 }
