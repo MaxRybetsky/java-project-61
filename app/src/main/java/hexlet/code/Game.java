0351e8f565cedfc4af7b7af203777e7011e9d304
MaxRybetsky <stsdewar@ya.ru>
2024-04-07 01:07:59 +0300

    Step 9.1: Fix message

--- app/src/main/java/hexlet/code/Game.java
+++ app/src/main/java/hexlet/code/Game.java
@@ -2,9 +2,9 @@ package hexlet.code;
 
 public interface Game {
     String QUESTION_TEMPLATE = "Question: %s\nYour answer: ";
-    String CORRECT_ANSWER_MESSAGE = "Correct!";
+    String CORRECT_ANSWER_MESSAGE = "\nCorrect!";
     String WRONG_ANSWER_MESSAGE_TEMPLATE =
-            "'%s' is wrong answer ;(. Correct answer was '%s'.\nLet's try again, %s!\n";
+            "\n'%s' is wrong answer ;(. Correct answer was '%s'.\nLet's try again, %s!\n";
 
     String YES = "yes";
     String NO = "no";
